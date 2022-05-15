99c76c3181591c36f84e223d19e30464eaab1c9a
aakash.patel <aakash.patel@globalvoxinc.com>
2022-05-15 21:11:07 +0530

    FNO Stock list module

--- src/main/java/com/sky/controller/NiftyController.java
+++ src/main/java/com/sky/controller/NiftyController.java
@@ -1,11 +1,12 @@
 package com.sky.controller;
 
-import com.sky.service.DataPrepareService;
 import com.sky.entity.nse.ApplicationStaticData;
 import com.sky.entity.nse.Instrument;
 import com.sky.entity.nse.NSE;
 import com.sky.feign.FeignBuilder;
 import com.sky.feign.FeignClientNSENifty;
+import com.sky.service.DataPrepareService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,29 +14,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
-@RestController
-public class HomeController {
+@Api
+@RestController("")
+public class NiftyController {
 
     @Autowired
     FeignClientNSENifty feignClientNSENifty;
     @Autowired
     DataPrepareService dataPrepareService;
     public static Map<String, String> headersMap;
+
     {
-       headersMap = FeignBuilder.builder();
+        headersMap = FeignBuilder.builder();
     }
 
-    @GetMapping("/getniftydata")
-    public ResponseEntity<Instrument> getOptionChainAnalysis(@RequestParam("expiryDate")String expiryDate) throws Exception {
+    @GetMapping("/getexpirydates")
+    public ResponseEntity<List<String>> getExpiryDates() throws Exception {
         NSE nse = feignClientNSENifty.getLiveNiftyData(FeignBuilder.builder());
         System.out.println(nse);
         ApplicationStaticData.expiryDates = Arrays.asList(nse.getRecords().getExpiryDates());
 
-        if(!ApplicationStaticData.expiryDates.contains(expiryDate))
+        return ResponseEntity.ok(ApplicationStaticData.expiryDates);
+    }
+
+    @GetMapping("/getniftydata")
+    public ResponseEntity<Instrument> getOptionChainAnalysis(@RequestParam("expiryDate") String expiryDate) throws Exception {
+        NSE nse = feignClientNSENifty.getLiveNiftyData(FeignBuilder.builder());
+        System.out.println(nse);
+        ApplicationStaticData.expiryDates = Arrays.asList(nse.getRecords().getExpiryDates());
+
+        if (!ApplicationStaticData.expiryDates.contains(expiryDate))
             throw new Exception("Invalid Expiry Date");
 
-        return ResponseEntity.ok(dataPrepareService.prepareInstrumentData(nse,50, 200, false, expiryDate));
+        return ResponseEntity.ok(dataPrepareService.prepareInstrumentData(nse, 50, 200, false, expiryDate));
     }
 }
--- src/main/java/com/sky/utils/URLS.java
+++ src/main/java/com/sky/utils/URLS.java
@@ -2,4 +2,8 @@ package com.sky.utils;
 
 public class URLS {
     public static final String GET_FNO_STOCKS = "https://www1.nseindia.com/content/fo/fo_mktlots.csv";
+    public static final String GET_INDEX_OPTION_CHAIN = "https://www.nseindia.com/api/option-chain-indices?symbol=";
+    public static final String GET_STOCK_OPTION_CHAIN = "https://www.nseindia.com/api/option-chain-equities?symbol=";
+    public static final String GET_NIFTY_50_STOCKS = "https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050";
+    public static final String GET_STOCK_QUOTES = "https://www.nseindia.com/api/quote-equity?symbol=";
 }
